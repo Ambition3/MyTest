6b12b442edb14910bcfb1feaa7ce55c81e07392f
Ambition3 <3266201589@qq.com>
2022-06-11 22:38:30 +0800

    第六次上传
    人物动画
    人物死亡
    水花
    remover脚本

--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerCtrl : MonoBehaviour
 {
@@ -11,14 +12,21 @@ public class PlayerCtrl : MonoBehaviour
     public bool bFaceRight = true;
     public bool bJump = false;
     public float JumpForce = 100;
+    public AudioClip[] JumpClips;//随机声音
+
+    //public AudioSource audioSource;
+    public AudioMixer audioMixer;
+
     private Transform mGroundCheck;
     Animator anim;
+    float mVolume = 0;
 
     void Start()
     {
         HeroBody = GetComponent<Rigidbody2D>();
         mGroundCheck = transform.Find("GroundCheck");
         anim = GetComponent<Animator>();//获取animator
+        //audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -42,7 +50,7 @@ public class PlayerCtrl : MonoBehaviour
         {
             flip();
         }
-
+        //射线检测是通过按位与的操作进行而不是通过“==”操作进行判断
         if (Physics2D.Linecast(transform.position, mGroundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
         {
             if (Input.GetButtonDown("Jump"))
@@ -50,12 +58,21 @@ public class PlayerCtrl : MonoBehaviour
                 bJump = true;
             }
         }
-
+        Debug.DrawLine(transform.position, mGroundCheck.position, Color.red);
     }
     private void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            mVolume++;
+            audioMixer.SetFloat("MasterVolume", mVolume);
+        }
         if (bJump)
         {
+            int i = Random.Range(0, JumpClips. Length);
+            //audioSource.clip = JumpClips[i];
+            //audioSource.Play();
+            AudioSource.PlayClipAtPoint(JumpClips[i], transform.position);
             HeroBody.AddForce(Vector2.up * JumpForce);
             bJump = false;
             anim.SetTrigger("jump");
